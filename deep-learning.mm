fc953bc01004149de6f517ba4eeb49211af988be
andyplusplus <wuzengde@vip.sina.com>
2017-11-13 15:25:23 +0800

    update

---
+++ deep-learning.mm
@@ -0,0 +1,164 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Deep Learning&#xa;&#x6df1;&#x5ea6;&#x5b66;&#x4e60;" FOLDED="false" ID="ID_171579464" CREATED="1491556975105" MODIFIED="1508374019380" STYLE="oval">
+<font SIZE="14"/>
+<hook NAME="MapStyle" zoom="2.852">
+    <properties fit_to_viewport="false;" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="23" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="&#x6df1;&#x5ea6;&#x5b66;&#x4e60;&#x7b97;&#x6cd5;" POSITION="right" ID="ID_132495871" CREATED="1508374651773" MODIFIED="1508376661743">
+<edge COLOR="#7c007c"/>
+<node TEXT="CNN" ID="ID_93421855" CREATED="1508374680769" MODIFIED="1508374683617"/>
+<node TEXT="RNN" ID="ID_1118955079" CREATED="1508374684156" MODIFIED="1508374686767"/>
+<node TEXT="LSTM" ID="ID_681795950" CREATED="1508374688011" MODIFIED="1508374690292"/>
+<node TEXT="SVM, GBDT" ID="ID_803376735" CREATED="1508376041271" MODIFIED="1508376045382"/>
+</node>
+<node TEXT="&#x673a;&#x5668;&#x5b66;&#x4e60;&#x7b97;&#x6cd5;" POSITION="right" ID="ID_1103862843" CREATED="1508376662266" MODIFIED="1508376666288">
+<edge COLOR="#7c7c00"/>
+<node TEXT="&#x5982;&#x903b;&#x8f91;&#x56de;&#x5f52;" ID="ID_450356130" CREATED="1508376687655" MODIFIED="1508376687655"/>
+<node TEXT="SVM" ID="ID_1211550136" CREATED="1508376687655" MODIFIED="1508376687655"/>
+<node TEXT="&#x795e;&#x7ecf;&#x7f51;&#x7edc;" ID="ID_199395723" CREATED="1508376687656" MODIFIED="1508376687656"/>
+<node TEXT="&#x51b3;&#x7b56;&#x6811;" ID="ID_511829123" CREATED="1508376687657" MODIFIED="1508376687657"/>
+<node TEXT="&#x8d1d;&#x53f6;&#x65af;&#x7b49;" ID="ID_849701632" CREATED="1508376687657" MODIFIED="1508376687657"/>
+</node>
+<node TEXT="&#x76f8;&#x5173;&#x6846;&#x67b6;" POSITION="right" ID="ID_1093409008" CREATED="1508374138437" MODIFIED="1508374138441">
+<edge COLOR="#00ffff"/>
+<node TEXT="tensorflow" ID="ID_1931861375" CREATED="1508374142851" MODIFIED="1508374142851"/>
+<node TEXT="caffe" ID="ID_46843196" CREATED="1508374142845" MODIFIED="1508374142845"/>
+<node TEXT="torch, theano, mxnet, keras" ID="ID_842141940" CREATED="1508374142845" MODIFIED="1508375509525"/>
+</node>
+<node TEXT="TODO" POSITION="left" ID="ID_1232996894" CREATED="1508374165326" MODIFIED="1508374168161">
+<edge COLOR="#7c0000"/>
+<node TEXT="&#x5185;&#x90e8;&#x57f9;&#x8bad;" ID="ID_273304216" CREATED="1508374169271" MODIFIED="1508374174942"/>
+<node TEXT="&#x5916;&#x90e8;&#x4ea4;&#x6d41;" ID="ID_346715116" CREATED="1508374175735" MODIFIED="1508374190917"/>
+<node TEXT="&#x8bba;&#x6587;&#x4e0e;&#x4e13;&#x5229;" ID="ID_590318815" CREATED="1508374435475" MODIFIED="1508374442029"/>
+</node>
+<node TEXT="&#x5e94;&#x7528;" POSITION="left" ID="ID_1330650550" CREATED="1508374394165" MODIFIED="1508374397886">
+<edge COLOR="#00007c"/>
+<node TEXT="&#x667a;&#x80fd;&#x95ee;&#x7b54;, &#x8bed;&#x97f3;&#x8bc6;&#x522b;, &#x81ea;&#x7136;&#x8bed;&#x8a00;&#x5904;&#x7406;NLP" ID="ID_1765638896" CREATED="1508374399279" MODIFIED="1508376172807"/>
+<node TEXT="&#x8ba1;&#x7b97;&#x673a;&#x89c6;&#x89c9;" ID="ID_609370899" CREATED="1508374499013" MODIFIED="1508374503213">
+<node TEXT="&#x7269;&#x4f53;&#xff08;&#x4eba;&#x4f53;&#x3001;&#x4eba;&#x8138;&#x3001;&#x901a;&#x7528;&#x76ee;&#x6807;&#xff09;&#x68c0;&#x6d4b;&#x3001;&#x8ddf;&#x8e2a;&#x4e0e;&#x8bc6;&#x522b;" ID="ID_75963593" CREATED="1508374562058" MODIFIED="1508374755752"/>
+<node TEXT="&#x573a;&#x666f;&#x7406;&#x89e3;&#xff0c;&#x884c;&#x4e3a;&#x5206;&#x6790; &#xff0c;&#x8bed;&#x4e49;&#x5206;&#x5272;" ID="ID_460711502" CREATED="1508374734485" MODIFIED="1508374763117"/>
+<node TEXT="OCR&#x8bc6;&#x522b;" ID="ID_583716767" CREATED="1508375017588" MODIFIED="1508375024279"/>
+</node>
+<node TEXT="" ID="ID_1776910682" CREATED="1508374503467" MODIFIED="1508376159471"/>
+<node TEXT="&#x81ea;&#x52a8;&#x9a7e;&#x9a76;" ID="ID_384912333" CREATED="1508374531188" MODIFIED="1508374540068"/>
+<node TEXT="&#x641c;&#x7d22;&#x5f15;&#x64ce;" ID="ID_1996818287" CREATED="1508374627863" MODIFIED="1508374636758"/>
+</node>
+<node TEXT="&#x5176;&#x5b83;&#x6280;&#x672f;&#x6846;&#x67b6;" POSITION="right" ID="ID_1103489615" CREATED="1508374584603" MODIFIED="1508374590903">
+<edge COLOR="#007c00"/>
+<node TEXT="Open CV" ID="ID_1804411059" CREATED="1508374592642" MODIFIED="1508374600038"/>
+<node TEXT="Docker" ID="ID_621506479" CREATED="1508374620048" MODIFIED="1508374623948"/>
+</node>
+<node TEXT="&#x6570;&#x636e;&#x96c6;" POSITION="right" ID="ID_243722279" CREATED="1508375259897" MODIFIED="1508375262371">
+<edge COLOR="#007c7c"/>
+<node TEXT="ImageNet" ID="ID_340743048" CREATED="1508375263375" MODIFIED="1508375266751"/>
+</node>
+<node TEXT="&#x76f8;&#x5173;&#x9886;&#x57df;" POSITION="left" ID="ID_1642650326" CREATED="1508374095846" MODIFIED="1508374104037">
+<edge COLOR="#ff00ff"/>
+<node TEXT="&#x4eba;&#x5de5;&#x667a;&#x80fd;, &#x6df1;&#x5ea6;&#x5b66;&#x4e60;, &#x6a21;&#x5f0f;&#x8bc6;&#x522b;, &#x673a;&#x5668;&#x5b66;&#x4e60;, SLAM" ID="ID_1940339477" CREATED="1508374104658" MODIFIED="1508376106492"/>
+<node TEXT="&#x5927;&#x6570;&#x636e;&#x53ca;&#x5206;&#x5e03;&#x5f0f;&#x7cfb;&#x7edf;" ID="ID_1495613657" CREATED="1508374104671" MODIFIED="1508374104671">
+<node TEXT="Hadoop" ID="ID_792507338" CREATED="1508374298512" MODIFIED="1508374298512"/>
+<node TEXT="Spark" ID="ID_1852592184" CREATED="1508374298512" MODIFIED="1508374298512">
+<node TEXT="Spark MLlib" ID="ID_1454251972" CREATED="1508375377601" MODIFIED="1508375384141"/>
+</node>
+<node TEXT="CDH" ID="ID_752391598" CREATED="1508374321864" MODIFIED="1508374323426"/>
+</node>
+<node TEXT="&#x5f02;&#x6784;&#x8ba1;&#x7b97;" ID="ID_1142499108" CREATED="1508374476483" MODIFIED="1508374480864">
+<node TEXT="GPU" ID="ID_605308344" CREATED="1508374481942" MODIFIED="1508374484929"/>
+<node TEXT="FPGA" ID="ID_1086026721" CREATED="1508374485543" MODIFIED="1508374487899"/>
+</node>
+<node TEXT="DeepMind&#x7684;&#x884c;&#x4e3a;&#x7b56;&#x7565;&#x81ea;&#x52a8;&#x5b66;&#x4e60;" ID="ID_614816174" CREATED="1508374348204" MODIFIED="1508374382675"/>
+</node>
+<node TEXT="autonomous Driving&#xa;self driving" POSITION="left" ID="ID_108578005" CREATED="1508376884460" MODIFIED="1508376940350">
+<edge COLOR="#ff0000"/>
+<node TEXT="autoware, ROS" ID="ID_410519579" CREATED="1508376945989" MODIFIED="1508377031834"/>
+<node TEXT="CUDA, OpenCL" ID="ID_94237557" CREATED="1508377015499" MODIFIED="1508377025624"/>
+<node TEXT="&#x76f8;&#x5173;&#x6280;&#x672f;" ID="ID_750073944" CREATED="1508377327064" MODIFIED="1508377331291">
+<node TEXT="1&#x3001;&#x8d1f;&#x8d23;ADAS&#x7b97;&#x6cd5;&#x5f00;&#x53d1;&#x5de5;&#x4f5c;&#xff1b;" ID="ID_1714153644" CREATED="1508377331984" MODIFIED="1508377331984"/>
+<node TEXT="2&#x3001;&#x5404;&#x79cd;&#x9053;&#x8def;&#x4ea4;&#x901a;&#x6307;&#x793a;&#x724c;&#x8bc6;&#x522b;&#xff1b;" ID="ID_1518139751" CREATED="1508377331984" MODIFIED="1508377331984"/>
+<node TEXT="3&#x3001;&#x8f66;&#x9053;&#x68c0;&#x6d4b;&#x53ca;&#x8f66;&#x9053;&#x504f;&#x79bb;&#x9884;&#x8b66;&#x7b97;&#x6cd5;&#xff1b;" ID="ID_1246598836" CREATED="1508377331986" MODIFIED="1508377331986"/>
+<node TEXT="4&#x3001;&#x8f66;&#x8f86;&#x3001;&#x884c;&#x4eba;&#x7b49;&#x5404;&#x79cd;&#x76ee;&#x6807;&#x7684;&#x68c0;&#x6d4b;&#xff1b;" ID="ID_1698055295" CREATED="1508377331988" MODIFIED="1508377331988"/>
+<node TEXT="5&#x3001;&#x57fa;&#x4e8e;&#x5355;/&#x53cc;&#x76ee;&#x6444;&#x50cf;&#x5934;&#x56fe;&#x50cf;&#x6d4b;&#x8ddd;&#xff1b;" ID="ID_1491763057" CREATED="1508377331989" MODIFIED="1508377331989"/>
+<node TEXT="&#x8ba1;&#x7b97;&#x673a;&#x89c6;&#x89c9;&#x3001;&#x6a21;&#x5f0f;&#x8bc6;&#x522b;&#x3001;&#x56fe;&#x50cf;&#x5904;&#x7406;&#x3001;&#x8ba1;&#x7b97;&#x673a;&#x89c6;&#x89c9;&#x3001;&#x6570;&#x5b66;" ID="ID_1371318527" CREATED="1508377331990" MODIFIED="1508377331990"/>
+<node TEXT="&#x7279;&#x5f81;&#x63d0;&#x53d6;&#x3001;&#x76ee;&#x6807;&#x68c0;&#x6d4b;&#x3001;&#x5355;&#x3001;&#x591a;&#x76ee;&#x6807;&#x8ddf;&#x8e2a;&#x7684;&#x4e3b;&#x6d41;&#x7b97;&#x6cd5;&#x3001;&#x719f;&#x6089;&#x76f8;&#x673a;&#x6807;&#x5b9a;&#x7b97;&#x6cd5;&#xff1b;" ID="ID_340931768" CREATED="1508377331991" MODIFIED="1508377331991"/>
+</node>
+</node>
+</node>
+</map>
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Deep Learning
+* [Make your own neural network](https://github.com/makeyourownneuralnetwork/makeyourownneuralnetwork)
+	* [Part 2]()	
+		* [part2_mnist_data_set.py](https://github.com/makeyourownneuralnetwork/makeyourownneuralnetwork/blob/master/part2_mnist_data_set.ipynb): basic demonstations of data read and write
+		* [part2_neural_network.ipynb](https://github.com/makeyourownneuralnetwork/makeyourownneuralnetwork/blob/master/part2_neural_network.ipynb)	: neural Network 
+		* [part2_neural_network_mnist_data.py](https://github.com/makeyourownneuralnetwork/makeyourownneuralnetwork/blob/master/part2_neural_network_mnist_data.ipynb): 100 train, 10 tests, 5 epoch: 60% ~ 70%, 15 epoch: 70% ~ 80%
+	* [Part 3]()
+		* [part3_load_own_images.ipynb]() : load your own images
+		* [part3_mnist_data_set_with_rotations.ipynb]() : rotate images
+		* [part3_neural_network_mnist_and_own_data.ipynb]() : test your own data
+		* [part3_neural_network_mnist_and_own_single_image.ipynb]() : same as previous
+		* [part3_neural_network_mnist_backquery.ipynb]() : neural network backquery
+		* [part3_neural_network_mnist_data_with_rotations.ipynb]() : network with rotations		
+* []()
+* []()
+* []()
+* []()
+# Keynotes
+* [深度学习难在哪里？](http://blog.csdn.net/yingyujianmo/article/details/51039348)
+	* 可以自己去读读近些年来在NIPS, CVPR, ACL, EMNLP等各领域顶级会议上发表的deep learning相关论文（arxiv还没发表的不算），可以说绝大部分论文要么是在模型上有所创新，要么是把深度学习用在了一个全新的问题上，单纯靠堆机器堆深度搭框架来提高性能的论文我还没怎么读到过。
+	* 深度学习需要的数学基础(凸优化，数值分析，矩阵分析，统计推断，概率图)把99%的程序员学生挡在了门外； 
+	* C/C++，CUDA/OpenCL，计算机网络和体系结构，大规模并行系统的门槛又将纯数学背景的限制在了只能用matlab写点两三层几千维神经网络的实验小程序上，而数据量不到100G深度不到五六层维度没有十万根本感受不到深度学习的可怕威力。 
+	* 所以两个知识的交叉领域还剩多少人？我估计全中国具有可实用深度学习知识的人应该只有300个左右。
+* [深度学习如何入门？](https://www.zhihu.com/question/26006703)
+* []()
+
+# Deep Learning Course
+* [Neural Networks for Machine Learning, Geoffrey Hinton, University of Toronto](https://class.coursera.org/neuralnets-2012-001)
+	* [Neural Networks for Machine Learning, youtube](https://www.youtube.com/watch?v=cbeTc-Urqak&list=PLoRl3Ht4JOcdU872GhiYWf6jwrk_SNhz9)
+	* [Youtube](https://www.youtube.com/user/aicourses)
+* []()
+* []()
+* []()
+* []()
+* []()
+* []()
+* []()
